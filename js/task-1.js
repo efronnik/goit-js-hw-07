37cb3423bae4a4fdea1b680d1337dd8d8bd6825e
Nikita <nikitaefron06@gmail.com>
2024-01-15 02:14:26 +0100

    Summary (required)

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,10 @@
-let myElement = document.getElementById('categories');
+const myElement = document.querySelectorAll('#categories .item');
+console.log('Number of categories:', myElement.length);
 
-console.log(myElement);
+myElement.forEach(function (item) {
+  const categoryName = item.querySelector('h2').textContent;
+  const categoryCount = item.querySelectorAll('li').length;
+
+  console.log(`Category: ${categoryName}
+  Elements: ${categoryCount}`);
+});
